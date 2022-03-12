83cb1dc1ab54163b1ad931b21ff4296d01621a2e
victorlucasrobs <victorlsa2002@gmail.com>
2022-03-12 18:47:25 -0300

    test

--- src/Core/DiscordBot.php
+++ src/Core/DiscordBot.php
@@ -1,21 +1,31 @@
 <?php
 
-namespace TavernaDeMordor\Bot\Core;
+namespace TavernaDeMordor\Bot\Php\Core;
 
 use Discord\Discord;
+use Discord\Parts\Channel\Message;
+use Discord\WebSockets\Event;
 
 class DiscordBot
 {
-  private Discord $client;
+  public Discord $client;
+  // private string $prefix;
 
   public function __construct(Discord $client)
   {
     $this->client = $client;
+    // $this->prefix = $prefix;
   }
 
   public function start()
   {
-    print("Rodando");
+    print("RODANDO");
     $this->client->run();
+  }
+
+  public function registerAction(string $action, callable $callback,)
+  {
+    $this->client->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+    });
   }
 }
